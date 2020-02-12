a29eb6482b36a205aad379b4c51a0fa199fb8ae7
SzzS <info@szarkazsolt.hu>
2020-02-12 22:28:37 +0100

    Debian version of the ttf-ibm-plex, vim-airline and vim-airline-themes installation is added.

--- install.sh
+++ install.sh
@@ -2,11 +2,14 @@
 
 dtstr="$(date +'%Y%m%d%H%M%S')"
 
-if [ -x /bin/pacman ]; then
+if [ -x /bin/pacman | -x /usr/bin/pacman ]; then
 	#Optional True Type Font for terminal window
 	sudo pacman -S --needed ttf-ibm-plex
 	#VIM Airline & Airline themes
 	sudo pacman -S --needed vim-airline vim-airline-themes
+elif [ -x /bin/apt | -x /usr/bin/apt ]; then
+	sudo apt install ttf-ibm-plex
+	sudo apt install vim-airline vim-airline-themes
 fi
 
 if [ -d "$HOME/.config/mc" ]; then
