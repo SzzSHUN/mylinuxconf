efeded3e19ef0b5efeef1ad674bc80873c292d66
SzzS <info@szarkazsolt.hu>
2020-02-12 22:32:33 +0100

    Package manager binary path check is modified.

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 dtstr="$(date +'%Y%m%d%H%M%S')"
 
-if [ -x /bin/pacman | -x /usr/bin/pacman ]; then
+if [ -x /bin/pacman ] || [ -x /usr/bin/pacman ]; then
 	#Optional True Type Font for terminal window
 	sudo pacman -S --needed ttf-ibm-plex
 	#VIM Airline & Airline themes
 	sudo pacman -S --needed vim-airline vim-airline-themes
-elif [ -x /bin/apt | -x /usr/bin/apt ]; then
+elif [ -x /bin/apt ] || [ -x /usr/bin/apt ]; then
 	sudo apt install ttf-ibm-plex
 	sudo apt install vim-airline vim-airline-themes
 fi
